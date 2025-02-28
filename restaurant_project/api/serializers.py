04c8512d6d9466d357de64ab32067be908b77719
Snehasish1099 <snehasishmondal987@gmail.com>
2025-02-28 23:33:00 +0530

    Fixed the resistration model

--- restaurant_project/api/serializers.py
+++ restaurant_project/api/serializers.py
@@ -2,26 +2,41 @@ from rest_framework import serializers
 from .models import Restaurant, MenuItem, Customer, Order
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth import get_user_model
 
-class UserSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class ProfileSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    phone_number = serializers.CharField(source="profile.phone_number", required=True)
+    address = serializers.CharField(source="profile.address", required=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'phone_number', 'address', 'password']
 
     def create(self, validated_data):
+        profile_data = validated_data.pop('profile')  # Extract profile fields
         user = User.objects.create_user(**validated_data)
-        Profile.objects.create(user=user)  # Automatically create Profile when user is created
+        Profile.objects.create(user=user, **profile_data)  # Create profile
         return user
-    
 
-class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', {})
+        instance.username = validated_data.get('username', instance.username)
 
-    class Meta:
-        model = Profile
-        fields = ['user', 'phone_number', 'address', 'created_at']
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+
+        instance.save()
+
+        # Update profile fields
+        profile = instance.profile
+        profile.phone_number = profile_data.get('phone_number', profile.phone_number)
+        profile.address = profile_data.get('address', profile.address)
+        profile.save()
+
+        return instance
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
--- restaurant_project/api/urls.py
+++ restaurant_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerView, MenuView, OrderView, RestaurantView, RegisterView, logout_view, LoginView
+from .views import CustomerView, MenuView, OrderView, RestaurantView, RegisterView, logout_view, LoginView, UserProfileListView, UserDetailView
 
 router = DefaultRouter()
 router.register(r'customers', CustomerView)
@@ -13,4 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path('users/', UserProfileListView.as_view(), name='registered_users'),
+    path('users/<int:id>/', UserDetailView.as_view(), name='user-detail')
 ]
