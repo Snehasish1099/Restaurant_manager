241e798095e95faf670102a7965134b8ede6df77
Snehasish1099 <snehasishmondal987@gmail.com>
2025-03-18 15:16:49 +0530

    merged order and orderitem in one for django admin

--- restaurant_project/api/admin.py
+++ restaurant_project/api/admin.py
@@ -1,23 +1,10 @@
 from django.contrib import admin
 from .models import Profile, MenuItem, Order, OrderItem, Restaurant, Review
 
-# admin.site.register(Restaurant)
-# admin.site.register(Review)
-# admin.site.register(Profile)
-# admin.site.register(MenuItem)
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'address')
-    search_fields = ('user__username', 'address')
-    list_filter = ('address',)
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'restaurant')
-    search_fields = ('name', 'restaurant__name')
-    list_filter = ('restaurant',)
-    ordering = ('price',)
+admin.site.register(Restaurant)
+admin.site.register(Review)
+admin.site.register(Profile)
+admin.site.register(MenuItem)
 
 class OrderItemInline(admin.TabularInline):  # or admin.StackedInline
     model = OrderItem
@@ -29,17 +16,4 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ('customer__username',)
     list_filter = ('created_at',)
     inlines = [OrderItemInline]  # Show Order Items within Order
-    ordering = ('-created_at',)
-
-@admin.register(Restaurant)
-class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address')
-    search_fields = ('name',)
-    list_filter = ('address',)
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('user', 'restaurant', 'rating', 'created_at')
-    search_fields = ('user__username', 'restaurant__name')
-    list_filter = ('rating', 'created_at')
     ordering = ('-created_at',)
